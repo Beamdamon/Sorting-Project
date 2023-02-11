ec1027a3331f10965ac43cd7cf8691aa97b58630
Beamdamon <Beamdamon@gmail.com>
2023-02-10 22:32:20 -0700

    Added insertion sort function

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,1 +1,20 @@
 #include "InsertionSort.h"
+
+void InsertionSort::insertionSort(int arr[], int n)
+{
+	int i, j, key;
+	
+	for (i = 1; i < n; i++)
+	{
+		
+		key = arr[i];
+		j = i - 1;
+
+		while (j >= 0 && arr[j] > key)
+		{
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = key;
+	}
+}
